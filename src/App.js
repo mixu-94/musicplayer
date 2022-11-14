ff1d05c16ec7b3e43e66eda90f41da0c6e1b6576
Marco Kadlubski <marco.kdlbsk@mailbox.org>
2022-11-14 14:59:16 +0100

    output footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
 import "./App.css";
 import Card from "./components/Card/Card";
 import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
@@ -11,7 +11,7 @@ function App() {
         <Card />
         <MusicPlayer />
       </main>
-
+      <Footer />
     </div>
   );
 }
