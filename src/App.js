6d21c02ed7e499650cc6743d5696dd043d4782b5
Marco Kadlubski <marco.kdlbsk@mailbox.org>
2022-11-14 14:15:29 +0100

    add main section

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Card />
-      <MusicPlayer />
+      <main>
+        <Card />
+        <MusicPlayer />
+      </main>
     </div>
   );
 }
