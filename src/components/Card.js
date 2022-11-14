8c5b1e3db46d456d70d9cb4bdef3731710017d03
valeviit <valeviit@icloud.com>
2022-11-14 12:14:36 +0100

    edit card component and styled

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Card = () => {
     return (
-        <div>
-            
+        <div className='card-style'>
+            <img className='card-img' src='https://www.theweeknd.com/sites/g/files/aaj15051/f/styles/suzuki_breakpoints_image_mobile-lg_sq/public/release/201609/818cf2dcae465de2c48c791829b1ca03606989a1?itok=BlI82QMA'></img>
+            <div className='card-text-container'>
+                <h4 className='card-text'>Blind ....</h4>
+                <p className='card-text'>The weekend</p>
+            </div>
+            <span class="material-symbols-outlined">
+                more_vert
+            </span>
         </div>
     );
 }
