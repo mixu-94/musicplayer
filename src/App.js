b28044df52f03da61f5ee8aa4ac2282a9146ec75
Sivak Hico <strukggg@gmail.com>
2022-11-14 14:14:15 +0100

    fetch the data from the API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.napster.com/v2.1/tracks/top?apikey=ZTk2YjY4MjMtMDAzYy00MTg4LWE2MjYtZDIzNjJmMmM0YTdm')
+      .then((response) => response.json())
+      .then((response) => setData(response.tracks))
+      .catch((err) => console.error(err))
+  }, [])
+
+  return (
+    <div className="App">
+      <ul>
+        {data.map((item) => (
+          <li key={item.albumId}>{`Artist Name: ${item.artistName}`}</li>
+        ))}
+      </ul>
+    </div>  
+  )
 }
-
-export default App;
